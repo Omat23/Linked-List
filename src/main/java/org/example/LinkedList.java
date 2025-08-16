652ce4abd21f6f1b870da561c85f03518352326f
OmarGonzalez <omendezcarreon@gmail.com>
2025-08-15 22:19:35 -0600

    Class LinkedList created

---
+++ src/main/java/org/example/LinkedList.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class LinkedList {
+    
+    private NodeLinkedList starLinkedList;
+    private NodeLinkedList endLinkedList;
+
+    public LinkedList(){
+        this.starLinkedList = null;
+        this.endLinkedList = null;
+    }
+}
