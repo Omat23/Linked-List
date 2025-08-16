4560af3bd95474e0b5c9a635263cb52302c560ac
OmarGonzalez <omendezcarreon@gmail.com>
2025-08-15 22:45:30 -0600

    Methods to addElements added

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -1,12 +1,35 @@
 package org.example;
 
+import javax.swing.JOptionPane;
+
 public class LinkedList {
     
-    private NodeLinkedList starLinkedList;
+    private NodeLinkedList startLinkedList;
     private NodeLinkedList endLinkedList;
 
     public LinkedList(){
-        this.starLinkedList = null;
+        this.startLinkedList = null;
         this.endLinkedList = null;
+    }
+
+    public void addElementsAsStack(){
+        int elementToSave = Integer.parseInt(JOptionPane.showInputDialog("Enter the element to save"));
+        NodeLinkedList newNodeLinkedList = new NodeLinkedList(elementToSave, this.startLinkedList);
+        this.startLinkedList = newNodeLinkedList;
+        if(this.endLinkedList == null){
+            this.endLinkedList = this.startLinkedList;
+        }
+    }
+
+    public void addElementsAsQueue(){
+        int elementToSave = Integer.parseInt(JOptionPane.showInputDialog("Enter the element to save"));
+        NodeLinkedList newNodeLinkedList = new NodeLinkedList(elementToSave);
+        if(this.endLinkedList == null){
+            this.startLinkedList = newNodeLinkedList;
+            this.endLinkedList = this.startLinkedList;
+        }else{
+            this.startLinkedList.setNextPositionNodeLinkedList(newNodeLinkedList);
+            this.startLinkedList = this.startLinkedList.getNextPositionNodeLinkedList();
+        }
     }
 }
